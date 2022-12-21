7e850e5de295332bfe5efd7db3a88c56ad0bd33c
umeshn95 <umeshnamdev9@gmail.com>
2022-12-21 23:50:48 +0530

    testapi2

--- controllers/fileuploaderController.js
+++ controllers/fileuploaderController.js
@@ -21,21 +21,6 @@ const singleFileUpload = async (req, res, next) => {
 }
 const multipleFileUpload = async (req, res, next) => {
     try{
-        let filesArray = [];
-        req.files.forEach(element => {
-            const file = {
-                fileName: element.originalname,
-                filePath: element.path,
-                fileType: element.mimetype,
-                fileSize: fileSizeFormatter(element.size, 2)
-            }
-            filesArray.push(file);
-        });
-        const multipleFiles = new MultipleFile({
-            title: req.body.title,
-            files: filesArray
-        });
-        await multipleFiles.save();
         res.status(201).send('Files Uploaded Successfully');
     }catch(error) {
         res.status(400).send(error.message);
